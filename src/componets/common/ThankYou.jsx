640254a7c79cda5ab007a47e027ffde5da420504
remeessalam <remeesreme4u@gmail.com>
2024-12-19 15:26:47 +0530

    thankyou page text color changed

--- src/componets/common/ThankYou.jsx
+++ src/componets/common/ThankYou.jsx
@@ -7,7 +7,7 @@ import { companyDetails, logoImg, routes } from "../../constant";
 const Thankyou = () => {
   return (
     <div className="min-h-screen flex flex-col justify-between bg-primary/20">
-      <div className="py-4 top-0 w-full bg-black/10 backdrop-blur-md z-50 text-white">
+      <div className="py-4 top-0 w-full bg-black/10 backdrop-blur-md z-50 text-black">
         <div className="wrapper flex justify-between items-center gap-10">
           <div className="flex justify-between items-center gap-20 w-full pl-[1rem] lg:pl-0">
             <Link to="/" className="cursor-pointer relative">
@@ -19,13 +19,13 @@ const Thankyou = () => {
           </div>
         </div>
       </div>
-      <div className="wrapper h-full flex flex-col items-center py-[8rem] gap-3 text-white text-center">
+      <div className="wrapper h-full flex flex-col items-center py-[8rem] gap-3 text-[#2e2e45] text-center">
         <h1 className="heading-1">Thank You!</h1>
-        <p className="desc">
+        <p className="desc !font-semibold">
           We have received your message and will get back to you as soon as
           possible.
         </p>
-        <Link to="/" className="primary-btn w-fit mt-4">
+        <Link to="/" className="primary-btn w-fit mt-4 text-black">
           Home
         </Link>
       </div>
